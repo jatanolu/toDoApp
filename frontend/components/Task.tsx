64cd5517575f70ebc924394a1da0633ba232f843
jatanolu <jacobrjean@gmail.com>
2023-02-02 13:25:17 -0500

    TODO-001 Display a list of tasks, displays static tasks from database

--- frontend/components/Task.tsx
+++ frontend/components/Task.tsx
@@ -1,22 +1,31 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 
-function Task() {
-	const [List, setList] = useState([])
-	const TaskList=async()=>{
-		let myResponse = await axios.get('tasks/')
+function Task(): JSX.Element {
+
+	const [List, setList] = useState<string[]>([])
+
+	interface Taskdata {
+		tasks: string[]
+		}
+
+	const TaskList = async() => {
+		let myResponse = await axios.get<Taskdata>('tasks/')
 		console.log(myResponse)	
 		setList(myResponse.data.tasks)
 	}
+
 	useEffect(()=>{
 		TaskList()
 	},[])
 	
 	return(
-	<>
-	<p>From the task component</p>
-	{List.map((task)=> {return (<p>{task}</p>)})}
-	</>
+		<>
+
+		<p>From the task component</p>
+		{List.map((task)=> {return (<p>{task}</p>)})}
+
+		</>
 	)
 }
 export default Task
